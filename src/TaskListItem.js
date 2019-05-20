01272b7b2bdcd1520004998d1c2e3c6a7c762d1f
github username <github email address>
2019-05-20 13:56:39 -0500

    We did it YAY

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,23 @@
 class TaskListItem {
-  
+  constructor(desc, name){
+      this.desc = desc
+      this.name = name
+  }
+
+  liTag(color){
+      let li = document.createElement('li')
+      li.innerText = this.desc
+      let deleteButton = document.createElement('button')
+      li.style.color = color
+      let p = document.createElement('p')
+      p.innerText = this.name
+      p.style.color = "black"
+      li.append(p)
+      deleteButton.innerText = "Finished Task"
+      li.append(deleteButton)
+      deleteButton.addEventListener("click", (e) => {
+        e.target.parentElement.remove()
+      })
+      return li 
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 
   const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
-
+  
+  const form = document.getElementById("create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let object = new TaskListItem(e.target[0].value, e.target[2].value)
+    const renderApp = () => (listContainer.append(taskList.render(object.liTag(e.target[1].value))))
+    // debugger
+    renderApp()
+    form.reset()
+  })
   // Add Form Behavior Here!
-
-  renderApp()
+  // let li = new TaskListItem(e)
+  // let ul = new TaskList(task)
+  
 });
