61622b6e5d88ab86befe6f489a9b5f20183436a6
github username <github email address>
2019-05-20 13:24:11 -0500

    Done.

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,31 @@
 class TaskListItem {
-  
+  constructor(task){
+    this.task = task;
+  }
+
+render(){
+  let li =  document.createElement('li')
+  li.innerText = this.task
+
+  let ddl =  document.createElement('button')
+  li.append(ddl)
+  ddl.name = "Delete"
+
+  ddl.innerText = "X"
+  ddl.addEventListener('click',function(e){
+    e.preventDefault()
+
+  let  content = e.target.parentElement
+  // debugger
+  content.remove()
+  })
+  // debugger
+let taskList = document.getElementById('tasks')
+  taskList.append(li)
 }
+
+}
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const renderApp = () => (listContainer.innerHTML = taskList.render());
 
   // Add Form Behavior Here!
+const form =  document.querySelector('#create-task-form')
+form.addEventListener('submit',function(e){
+  e.preventDefault()
+  let task =  new TaskListItem(e.target[0].value)
+  task.render()
+})
 
   renderApp()
 });
+
+
+
