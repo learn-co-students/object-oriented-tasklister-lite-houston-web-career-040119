ffcc2633abc5dcb74c846445fd6847bd1c4370e5
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-20 11:44:44 -0500

    done

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,21 @@
 class TaskListItem {
-  
+
+  constructor(description) {
+      this.description = description
+      this.li = document.createElement('li')
+
+  }
+
+
+  render(ul) {
+      this.li.innerText = this.description
+      ul.append(this.li)
+      let button = document.createElement("button")
+      button.innerText = "X"
+      this.li.append(button)
+
+      button.addEventListener("click", e => {
+          e.target.parentElement.style.display = "none"
+      })
+  }
 }
