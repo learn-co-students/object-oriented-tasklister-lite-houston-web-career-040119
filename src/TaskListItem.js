240f5e38988895e848d2af3cdbce286c6d6f8297
github username <github email address>
2019-05-23 16:29:48 -0500

    Done.

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,31 +1,101 @@
 class TaskListItem {
-  constructor(task){
-    this.task = task;
+  constructor(desc){
+    this.description = desc
   }
 
-render(){
-  let li =  document.createElement('li')
-  li.innerText = this.task
+  renderListItem(){
+    
+      let listitem = document.createElement("li")
+      listitem.innerText = this.description
 
-  let ddl =  document.createElement('button')
-  li.append(ddl)
-  ddl.name = "Delete"
+      let btn = document.createElement('button')
+      btn.innerText = 'X'  
+      btn.addEventListener("click", (e) => {
+        e.preventDefault()
+        e.target.parentElement.remove()
+    })
 
-  ddl.innerText = "X"
-  ddl.addEventListener('click',function(e){
-    e.preventDefault()
 
-  let  content = e.target.parentElement
-  // debugger
-  content.remove()
-  })
-  // debugger
-let taskList = document.getElementById('tasks')
-  taskList.append(li)
+      let selectTag = document.createElement('select')
+      let titleOpt = document.createElement("option")
+      titleOpt.innerText = "Select Priority"
+      titleOpt.selected = true
+      titleOpt.disabled = true
+      selectTag.append(titleOpt)
+      let arr = ["Low", "Medium", "High"]
+      arr.forEach((priority) => {
+        let opt = document.createElement('option')
+        opt.text  = priority
+        opt.value = priority
+        selectTag.append(opt)
+      })
+
+      selectTag.addEventListener("change", (e) => {
+        switch(e.target.value) {
+            case "High":
+              e.target.parentElement.style.color = "red"
+              break;
+            case "Medium":
+                e.target.parentElement.style.color = "yellow"
+              break;
+            case "Low":
+                e.target.parentElement.style.color = "green"
+              break;
+            default:
+              console.log("not valid selection") 
+          }
+
+      })
+
+
+      listitem.append(btn , selectTag)
+
+      TaskListItem.all.push(listitem)
+
+      return TaskListItem.all
+
+  }
+
+
+  static sortListItem(sort){
+
+        let sorted = []
+        let high = []
+        let medium = []
+        let low = []
+
+        TaskListItem.all.forEach((task) => {
+            switch(task.style.color){
+                case "red":
+                    high.push(task)
+                    break;
+                case "yellow":
+                    medium.push(task)
+                    break;
+                case "green":
+                    low.push(task)
+                    break;
+                default:
+                    console.log("not valid selection")
+            }
+
+        })
+
+      switch(sort){
+        case "ASC":
+            sorted = [...low, ...medium, ...high]
+            break;
+        case "DESC":
+            sorted = [...high, ...medium, ...low]
+            break;
+        default:
+            console.log("not valid selection")
+      }
+
+      return sorted
+  }
 }
 
-}
-
-
+TaskListItem.all = [] //can't define it before class because it won't know what TaskListIt
 
 
--- src/taskList.js
+++ src/taskList.js
@@ -1,14 +1,41 @@
-class TaskList {
-    render(){
-      let ul =  document.createElement('ul')
-      let h2 =  document.createElement('h2')
-      ul.id = 'tasks'
-      h2.innerText = "To do"
-      let div = document.querySelector('#list')
+// class TaskList {
+//     render(){ //
+//       let ul =  document.createElement('ul')
+//       let h2 =  document.createElement('h2')
+//       ul.id = 'tasks'
+//       h2.innerText = "To do"
+//       let div = document.querySelector('#list')
     
-      // div.append(h2,ul)
-      return h2.outerHTML + ul.outerHTML
-    }
+//       // div.append(h2,ul)
+//       return h2.outerHTML + ul.outerHTML
+//     }
   
   
+//   }
+
+class TaskList {
+
+  constructor(){
+      this.ul = document.createElement('ul')
+      this.h2 = document.createElement('h2')
+      this.h2.innerText = "TO-DO"
   }
+
+
+render(desc){
+   let taskitem = new TaskListItem(desc)
+   let liArray = taskitem.renderListItem()
+   liArray.forEach((li) => {
+       this.ul.append(li)
+   })
+}
+
+sorting(sort){
+  let liArray = TaskListItem.sortListItem(sort)
+  liArray.forEach((li) => {
+      this.ul.append(li)
+  })
+}
+
+
+}
